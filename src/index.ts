7c18bf5e6e6a689fd205d941b0a9ab7382781ae6
sqrtthree <imsqrtthree@gmail.com>
2021-07-20 11:41:46 +0800

    feat: make it work

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Koa from 'koa'
+import _ from 'lodash'
+
+type tokenFn = () => string | Promise<string>
+
+interface tokenOptions {
+  token: string | tokenFn
+}
+
+/**
+ * Verify requests with special token.
+ *
+ * @param options Options of token middleware.
+ * @param options.token string or function to get target token.
+ *
+ * @returns token middleware
+ */
+export function tokenMiddleware(options: tokenOptions): Koa.Middleware {
+  return async function middleware(
+    ctx: Koa.Context,
+    next: Koa.Next
+  ): Promise<void> {
+    let token = ctx.headers['x-token'] || ctx.headers.token
+
+    if (!token && ctx.headers.authorization) {
+      token = ctx.headers.authorization.substring('Bearer '.length)
+    }
+
+    if (!token) {
+      ctx.throw(403, 'Token is required')
+    }
+
+    let targetToken = ''
+
+    if (typeof options.token === 'string') {
+      targetToken = options.token
+    } else {
+      targetToken = await options.token()
+    }
+
+    const isValid = token === targetToken
+
+    if (!isValid) {
+      ctx.throw(401, 'Invalid token')
+    }
+
+    return next()
+  }
+}
